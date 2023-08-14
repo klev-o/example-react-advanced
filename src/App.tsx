8830fed59da2dee96e6caba67a98a882831ba2d7
KonstantinLev <x-stels@bk.ru>
2023-08-14 18:43:18 +0300

    Separated themes, added theme provider, added hook for use different themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Suspense } from 'react';
+import {Suspense} from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>THEME</button>
             <Link to={'/'}>Main page</Link>
             <Link to={'/about'}>About page</Link>
             <Suspense fallback={<div>Loading...</div>}>
