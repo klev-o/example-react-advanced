c35935192f5b32a5b78cd435333c733249248c2e
KonstantinLev <x-stels@bk.ru>
2023-08-18 17:29:06 +0300

    Refactored folder structure for FSD, configured absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import {Suspense} from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
-import {Counter} from "./components/Counter";
 import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -19,11 +18,10 @@ const App = () => {
             <Link to={'/about'}>About page</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
-            <Counter />
         </div>
     );
 };
