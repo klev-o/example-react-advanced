51636c361198904bac04e518ff7194a012485d31
KonstantinLev <x-stels@bk.ru>
2023-09-05 17:39:42 +0300

    stylelint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,50 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
+    env: {
+        browser: true,
+        es2021: true
     },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'plugin:i18next/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    extends: [
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'plugin:i18next/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.eslint.json'
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.eslint.json'
     },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'i18next'
-  ],
-  rules: {
-    'react/jsx-indent': ['error', 4],
-    'react/jsx-indent-props': ['error', 4],
-    'indent': ['error', 4],
-    '@typescript-eslint/indent': ['error', 4],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': 'warn',
-    'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'react/function-component-definition': 'off',
-    'no-shadow': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    "@typescript-eslint/strict-boolean-expressions": "warn",
-    "@typescript-eslint/prefer-nullish-coalescing": "warn",
-    'i18next/no-literal-string': ['error', {markupOnly: true}]
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next'
+    ],
+    rules: {
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        indent: ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'warn',
+        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+        'i18next/no-literal-string': ['error', { markupOnly: true }]
     },
-  globals: {
-    __IS_DEV__: true
-    },
+    globals: {
+        __IS_DEV__: true
+    }
 }
